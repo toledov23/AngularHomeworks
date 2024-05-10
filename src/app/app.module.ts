5bcd47875dd15e72112212bee185a9e22cf9fa34
Eduardo Toledo <eduardo_toledov@hotmail.com>
2024-05-09 19:52:29 -0600

    Directivas custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizeDirective } from './directives/capitalize.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizeDirective
   ],
   imports: [
     BrowserModule,
